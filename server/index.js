d5998a2600852b363d5363255a9f0ca02afc6155
Jacob Dayton <jacob@Jacobs-MacBook-Pro.local>
2018-06-21 19:29:03 -0600

    final

--- server/index.js
+++ server/index.js
@@ -2,16 +2,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
 const port = 1337;
+const createInitialSession = require('./middlewares/session')
+const session = require('express-session')
+const filterProfanity = require('./middlewares/filter')
+require('dotenv').config()
+
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use( session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 10000 }
+  }));
+
+app.use(createInitialSession)
+app.use( (req,res,next) => {
+    const { method } = req
+    if ( method === "POST" || method === "PUT") {
+        filterProfanity( req,res,next)
+    } else {
+        next()
+    }
+})
 
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
 app.put( "/api/messages", mc.update );
 app.delete( "/api/messages", mc.delete );
+app.get( "/api/messages/history", mc.history );
 
 
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
---
+++ server/middlewares/filter.js
@@ -0,0 +1,9 @@
+notAllowed = ['fuck', 'damn', 'shitass', 'damn', 'shit', 'ass']
+
+module.exports = (req,res,next) => {
+    while ( notAllowed.find( word => req.body.text.includes(word) ) ) {
+        const badWord = notAllowed.find( word => req.body.text.includes(word) );
+        req.body.text = req.body.text.replace( badWord, '*'.repeat( badWord.length ) );
+    }
+    next()
+}
